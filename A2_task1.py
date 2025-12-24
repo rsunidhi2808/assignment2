b52cab77c9464693d6708ddafcf2071b807386f3
rsunidhi2808 <rsunidhi2808@gmail.com>
2025-12-24 20:54:27 +0530

    Add files via upload

---
+++ A2_task1.py
@@ -0,0 +1,8 @@
+int1=int(input("Enter a number: "))
+if (int1%2)==0:
+    print(f"{int1} is an even number")
+
+else:
+    print(f"{int1} is a odd number")
+
+
---
+++ A2_task2.py
@@ -0,0 +1,4 @@
+sum_1=0
+for i in range(1,50):
+    sum_1= sum_1+i
+print(f"The sum of numbers from 1 to 50 is: {sum_1}")
